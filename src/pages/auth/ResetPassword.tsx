0ed0af1b3d0460926b09fb14e7d7d1ed07e67356
stevechude <stevechude@gmail.com>
2025-03-21 20:32:37 +0100

    checking again

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import Authlayout from "../../components/layouts/Authlayout";
 import { useState } from "react";
 import { Modal } from "../../components/modal/Modal";
-import ResetSuccessful from "../../components/cards/ResetSuccessful";
 import CardSuccessful from "../../components/cards/CardSuccessful";
 
 const ResetPassword = () => {
@@ -24,26 +23,26 @@ const ResetPassword = () => {
 
   return (
     <Authlayout>
-      <div className="flex flex-col gap-10 lg:gap-5 2xl:gap-10">
+      <div className="flex flex-col gap-10 lg:gap-5 xl:gap-10">
         <div className="flex flex-col items-center lg:items-start gap-2">
           <p className="text-primary font-semibold text-xl md:text-3xl">
             Reset Password
           </p>
-          <p className="text-secondary font-medium text-xs md:text-sm 2xl:text-base text-center lg:text-start">
+          <p className="text-secondary font-medium text-xs md:text-sm xl:text-base text-center lg:text-start">
             Enter your new password and confirm it below to reset your password.
           </p>
         </div>
 
-        <form className="flex flex-col gap-10 lg:gap-5 2xl:gap-10 w-full px-2 lg:p-0">
+        <form className="flex flex-col gap-10 lg:gap-5 xl:gap-10 w-full px-2 lg:p-0">
           <div className="flex flex-col gap-1">
             <label
               htmlFor="password"
-              className="text-primary text-sm 2xl:text-base"
+              className="text-primary text-sm xl:text-base"
             >
               Enter Password
             </label>
             <div className="flex items-center border border-[#E3EFFC] rounded-3xl bg-[#FCFCFD] w-full">
-              <div className="flex items-center gap-1 md:gap-2 w-full pr-3 lg:pr-4 text-xs md:text-sm 2xl:text-base">
+              <div className="flex items-center gap-1 md:gap-2 w-full pr-3 lg:pr-4 text-xs md:text-sm xl:text-base">
                 <input
                   type="password"
                   placeholder="Password"
@@ -58,12 +57,12 @@ const ResetPassword = () => {
           <div className="flex flex-col gap-1">
             <label
               htmlFor="password"
-              className="text-primary text-sm 2xl:text-base"
+              className="text-primary text-sm xl:text-base"
             >
               Confirm Password
             </label>
             <div className="flex items-center border border-[#E3EFFC] rounded-3xl bg-[#FCFCFD] w-full">
-              <div className="flex items-center gap-1 md:gap-2 w-full pr-3 lg:pr-4 text-xs md:text-sm 2xl:text-base">
+              <div className="flex items-center gap-1 md:gap-2 w-full pr-3 lg:pr-4 text-xs md:text-sm xl:text-base">
                 <input
                   type="password"
                   placeholder="Password"
@@ -77,14 +76,14 @@ const ResetPassword = () => {
 
           <button
             onClick={handleResetPassword}
-            className="bg-primary rounded-3xl py-2 2xl:py-3 w-full text-white cursor-pointer mt-10 text-xs md:text-sm 2xl:text-base"
+            className="bg-primary rounded-3xl py-2 xl:py-3 w-full text-white cursor-pointer mt-10 text-xs md:text-sm xl:text-base"
           >
             Continue
           </button>
         </form>
 
         <div className="flex items-center justify-center w-full">
-          <p className="text-secondary font-inter text-xs md:text-sm 2xl:text-base">
+          <p className="text-secondary font-inter text-xs md:text-sm xl:text-base">
             New user?{" "}
             <Link to={"/register"} className="text-tertiary">
               Click here to register
@@ -98,7 +97,7 @@ const ResetPassword = () => {
             header="Password Reset"
             description="Fantastic! Your password is now reset. Kindly proceed to the
             login page."
-            close={() => setIsSuccess(false)}
+            close={resetDone}
           />
         </Modal>
       )}
